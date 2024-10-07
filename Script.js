13161255e9e2be6cc4ab32467d7cc2f362d8dfbf
satya@praveen <kommoju.praveen2001@gmail.com>
2024-10-07 17:09:39 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,69 @@
+// // let JS = 'Amazing';
+// //     if(JS === "Amazing") alert("javaScript is Fun");
+
+
+// let firstNmae = 'Satya is here';
+// window.alert(firstNmae);
+
+
+// let game = true;
+// console.log(typeof game);
+
+
+// let country = 'India';
+// let contient ='Asia';
+// let population = 130
+
+// console.log (country);
+// console.log (contient);
+// console.log(population);
+
+
+// let age = 23
+// age = 24
+// console.log(age)
+
+
+// let x = 5 + 10;
+// x += 10;
+
+// --x;
+// --x;
+// console.log(x);
+
+
+// const massMark = 78;
+// const markHeight = 1.69;
+// const massJohn = 92;
+// const JohnHeight = 1.95;
+
+// const mark = massMark / markHeight **2;
+// const john = massJohn / JohnHeight **2;
+// console.log(mark, john);
+
+
+// const markHigherBMI = mark > john;
+// console.log(markHigherBMI);
+
+// if(mark < john ){
+//     console.log(`mark's BMI (${mark})is greater then john (${john})`)
+// }
+// else{
+//     console.log(`john BMI (${john}) is higher than mark (${mark})`)
+// }
+
+
+
+const dolphins = (96 + 108 + 89)/3;
+const koalas = (88 + 91 + 110)/3;
+console.log(dolphins,koalas);
+
+// if(dolphins > koalas && dolphins>=100) {
+//     console.log(`dolphins win the trophy`);
+// }else if(koalas > dolphins && koalas>=100){
+//     console.log(`koalas win the trophy`);
+// }else if(dolphins !== koalas && dolphins>=100 && dolphins>=100){
+//     console.log(`both  win the trophy`);
+// }else{
+//     console.log(`no one win the trophy`)
+// }
